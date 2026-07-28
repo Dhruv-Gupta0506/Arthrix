9ad353725574ebe1561beb0430a396ad715c2509
Dhruv-Gupta0506 <dhruvgupta0506@gmail.com>
2026-07-28 22:16:56 +0530

    Initial commit: arthrix setup

---
+++ src/main/java/com/dhruv/arthrix/ArthrixApplication.java
@@ -0,0 +1,13 @@
+package com.dhruv.arthrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArthrixApplication.class, args);
+	}
+
+}
